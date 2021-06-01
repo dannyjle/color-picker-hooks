d755e16507c57a958b8b2a96607ea7b11068aaeb
Danny Le <le.dannyj@gmail.com>
2021-06-01 13:46:17 -0400

    Added Range Selector for Hue, Sat, and Light

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
+import React, { useState } from 'react'
+
 export function App() {
+  const [hue, setHue] = useState(0)
+  const [saturation, setSaturation] = useState(0)
+  const [lightness, setLightness] = useState(0)
+
   return (
     <>
       <h1>Color Selector!</h1>
+      <div>Pick your color:</div>
+      <p>Hue:</p>
+      <input
+        type="range"
+        value={hue}
+        max={360}
+        min={0}
+        onChange={(event) => setHue(parseInt(event.target.value))}
+      />
+      <p>Saturation:</p>
+      <input
+        type="range"
+        value={saturation}
+        max={360}
+        min={0}
+        onChange={(event) => setSaturation(parseInt(event.target.value))}
+      />
+      <p>Lightness:</p>
+      <input
+        type="range"
+        value={lightness}
+        max={360}
+        min={0}
+        onChange={(event) => setLightness(parseInt(event.target.value))}
+      />
     </>
   )
 }
