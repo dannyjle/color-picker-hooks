fc267b9c446111ceb8a590be51b3048a40fa3fa6
Danny Le <le.dannyj@gmail.com>
2021-06-01 14:01:37 -0400

    Added CSS and Page Refresh Random

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
 import React, { useState } from 'react'
 
 export function App() {
-  const [hue, setHue] = useState(0)
-  const [saturation, setSaturation] = useState(0)
-  const [lightness, setLightness] = useState(0)
+  const [hue, setHue] = useState(Math.floor(Math.random() * (359 - 1) + 1))
+  const [saturation, setSaturation] = useState(
+    Math.floor(Math.random() * (100 - 1) + 1)
+  )
+  const [lightness, setLightness] = useState(
+    Math.floor(Math.random() * (100 - 1) + 1)
+  )
+
+  const newBackgroundColor = `hsl(${hue},${saturation}%,${lightness}%)`
+
+  const newStyle = { backgroundColor: newBackgroundColor }
 
   return (
     <>
-      <h1>Color Selector!</h1>
-      <div>Pick your color:</div>
-      <p>Hue:</p>
-      <input
-        type="range"
-        value={hue}
-        max={360}
-        min={0}
-        onChange={(event) => setHue(parseInt(event.target.value))}
-      />
-      <p>Saturation:</p>
-      <input
-        type="range"
-        value={saturation}
-        max={360}
-        min={0}
-        onChange={(event) => setSaturation(parseInt(event.target.value))}
-      />
-      <p>Lightness:</p>
-      <input
-        type="range"
-        value={lightness}
-        max={360}
-        min={0}
-        onChange={(event) => setLightness(parseInt(event.target.value))}
-      />
+      <main>
+        <h1>Color Selector!</h1>
+        <h4>Pick your color:</h4>
+        {/* <h5>
+          <button onClick={handleButtonClick}>Randomizer</button>
+        </h5> */}
+        <div className="colorBox" style={newStyle}>
+          {`hsl(${hue},${saturation}%,${lightness}%)`}
+        </div>
+        <article>
+          <p>Hue: {hue}</p>
+          <input
+            type="range"
+            value={hue}
+            max={360}
+            min={0}
+            onChange={(event) => setHue(parseInt(event.target.value))}
+          />
+          <p>Saturation: {saturation}</p>
+          <input
+            type="range"
+            value={saturation}
+            max={100}
+            min={0}
+            onChange={(event) => setSaturation(parseInt(event.target.value))}
+          />
+          <p>Lightness: {lightness}</p>
+          <input
+            type="range"
+            value={lightness}
+            max={100}
+            min={0}
+            onChange={(event) => setLightness(parseInt(event.target.value))}
+          />
+        </article>
+      </main>
     </>
   )
 }
